fa2e2f67c49dd305a25bdf2131bcfa84a10b87ac
Favixx <bogdandom52@gmail.com>
2023-05-24 14:27:09 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,7 @@ import { FriendList } from "./Friends";
 import TransactionHistory from "./TransactionHistory";
 export const App = () => {
   return (
-    <div>
+    <div className="container">
       <Profile
         username={user.username}
         tag={user.tag}
@@ -17,7 +17,6 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
-      <Statistics stats={data} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
     </div>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,26 +1,26 @@
-
+import styles from "../css/Profile.module.css"
 const Profile = ({ username, tag, location, avatar, stats }) => {
-    return (<div className="profile">
-        <div className="description">
-            <img className="avatar" src={avatar} alt="User avatar" />
-            <p className="name">{username}</p>
-            <p className="tag">@{tag}</p>
-            <p className="location">{location}</p>
+    return (<div className={styles.profile}>
+        <div className={styles.description}>
+            <img className={styles.avatar} src={avatar} alt='User avatar' />
+            <p className={styles.name}>{username}</p>
+            <p className={styles.tag}>@{tag}</p>
+            <p className={styles.location}>{location}</p>
         </div>
-        <ul className="stats">
+        <ul className={styles.stats}>
             <li>
-                <span className="label">Followers</span>
-                <span className="quantity">{stats.followers}</span>
+                <span className={styles.label}>Followers</span>
+                <span className={styles.quantity}>{stats.followers}</span>
             </li>
             <li>
-                <span className="label">Views</span>
-                <span className="quantity">{stats.views}</span>
+                <span className={styles.label}>Views</span>
+                <span className={styles.quantity}>{stats.views}</span>
             </li>
             <li>
-                <span className="label">Likes</span>
-                <span className="quantity">{stats.likes}</span>
+                <span className={styles.label}>Likes</span>
+                <span className={styles.quantity}>{stats.likes}</span>
             </li>
         </ul>
-    </div>)
+    </div >)
 }
 export default Profile;
