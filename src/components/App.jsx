e83a3c7431fc7d2c45950092cda73f191c611d6a
Favixx <bogdandom52@gmail.com>
2023-05-25 17:18:52 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
-import Profile from "./Profile";
 import user from 'data/user.json';
 import data from 'data/data.json';
 import friends from 'data/friends.json'
 import transactions from 'data/transactions.json'
+import Profile from "./Profile";
 import { Statistics } from "./Statistics";
 import { FriendList } from "./Friends";
 import TransactionHistory from "./TransactionHistory";
